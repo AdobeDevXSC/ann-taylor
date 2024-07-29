f6ee79f3135f978329c2ba5af8dd7a30977181df
KP Parrish <krparrish@adobe.com>
2024-07-29 12:55:00 -0600

    added fonts and global variables

---
+++ blocks/teaser/teaser.js
@@ -0,0 +1,22 @@
+export default function decorate(block) {
+	const blockImages = block.querySelectorAll('picture');
+
+	// media query match that indicates mobile/tablet width
+	const isDesktop = window.matchMedia('(min-width: 1024px)');
+
+	function toggleImage() {
+		const desktopImage = blockImages[0];
+		const mobileImage = blockImages[1];
+		
+		if(isDesktop.matches){
+			mobileImage.closest('div').className = 'hidden';
+			desktopImage.closest('div').className = '';
+		} else {
+			desktopImage.closest('div').className = 'hidden';
+			mobileImage.closest('div').className = ''
+		}
+	}
+
+	toggleImage();
+	isDesktop.addEventListener('change', () => toggleImage());
+  }
