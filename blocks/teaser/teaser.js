b8fdfb7caffda0dff94426cc7a53a9674b798e9e
KP Parrish <krparrish@adobe.com>
2024-08-13 10:12:44 -0600

    testing eager v lazy loading in teaser block

--- blocks/teaser/teaser.js
+++ blocks/teaser/teaser.js
@@ -1,5 +1,6 @@
 export default function decorate(block) {
-	const blockImages = block.querySelectorAll('picture');
+	const blockImages = block.querySelectorAll('img');
+	const isHero = block.classList.contains('hero');
 
 	// media query match that indicates mobile/tablet width
 	const isDesktop = window.matchMedia('(min-width: 1024px)');
@@ -11,12 +12,22 @@ export default function decorate(block) {
 		if(isDesktop.matches){
 			mobileImage.closest('div').className = 'hidden';
 			desktopImage.closest('div').className = '';
+			if(isHero){
+				desktopImage.removeAttribute('loading'); // Lighthouse recommendation: remove lazy-loading
+				desktopImage.setAttribute('loading', 'eager');
+			}
 		} else {
 			desktopImage.closest('div').className = 'hidden';
 			mobileImage.closest('div').className = ''
+			if(isHero){
+				mobileImage.removeAttribute('loading'); // Lighthouse recommendation: remove lazy-loading
+				mobileImage.setAttribute('loading', 'eager');
+			}
 		}
 	}
 	
 	toggleImage();
 	isDesktop.addEventListener('change', () => toggleImage());
   }
+
+  
