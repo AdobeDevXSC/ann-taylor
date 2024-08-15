e6bcaaadfbcccc72866db96ba12d2f24bc7a7474
KP Parrish <krparrish@adobe.com>
2024-08-15 11:13:55 -0600

    popout functions triggering apporopriately

--- blocks/curalate-carousel/curalate-carousel.js
+++ blocks/curalate-carousel/curalate-carousel.js
@@ -68,26 +68,6 @@ function bindEvents(block) {
   block.querySelectorAll('.carousel-slide').forEach((slide) => {
     slideObserver.observe(slide);
   });
-  
-}
-
-function createSlide(row, slideIndex, carouselId) {
-  const slide = document.createElement('li');
-  slide.dataset.slideIndex = slideIndex;
-  slide.setAttribute('id', `carousel-${carouselId}-slide-${slideIndex}`);
-  slide.classList.add('carousel-slide');
-
-  row.querySelectorAll(':scope > div').forEach((column, colIdx) => {
-    column.classList.add(`carousel-slide-${colIdx === 0 ? 'image' : 'content'}`);
-    slide.append(column);
-  });
-
-  const labeledBy = slide.querySelector('h1, h2, h3, h4, h5, h6');
-  if (labeledBy) {
-    slide.setAttribute('aria-labelledby', labeledBy.getAttribute('id'));
-  }
-
-  return slide;
 }
 
 async function fetchJson(link) {
@@ -99,6 +79,7 @@ async function fetchJson(link) {
   }
 	return 'an error occurred';
 }
+
 
 let carouselId = 0;
 export default async function decorate(block) {
@@ -138,50 +119,51 @@ export default async function decorate(block) {
 
     container.append(slideNavButtons);
   }
+  
 
-
-//  fetch data and render
+	// fetch and manage slide data from json and process related display elements
 	const link = block.querySelector('a');
   	const slideData = await fetchJson(link);
 
+	// individual slide structure
 	slideData.forEach((slide, idx) => {
 		const slideProductsData = [
-				// 1
-				{
-					'title': slide.title1,
-					'image': slide.image1,
-					'price': slide.price1,
-					'salePrice': slide.salePrice1,
-				},
-				// 2
-				{
-					'title': slide.title2,
-					'image': slide.image2,
-					'price': slide.price2,
-					'salePrice': slide.salePrice2,
-				},
-				// 3
-				{
-					'title': slide.title3,
-					'image': slide.image3,
-					'price': slide.price3,
-					'salePrice': slide.salePrice3,
-				},
-				// 4
-				{
-					'title': slide.title4,
-					'image': slide.image4,
-					'price': slide.price4,
-					'salePrice': slide.salePrice4,
-				},
-				// 5
-				{
-					'title': slide.title5,
-					'image': slide.image5,
-					'price': slide.price5,
-					'salePrice': slide.salePrice5,
-				}
-			];
+			// 1
+			{
+				'title': slide.title1,
+				'image': slide.image1,
+				'price': slide.price1,
+				'salePrice': slide.salePrice1,
+			},
+			// 2
+			{
+				'title': slide.title2,
+				'image': slide.image2,
+				'price': slide.price2,
+				'salePrice': slide.salePrice2,
+			},
+			// 3
+			{
+				'title': slide.title3,
+				'image': slide.image3,
+				'price': slide.price3,
+				'salePrice': slide.salePrice3,
+			},
+			// 4
+			{
+				'title': slide.title4,
+				'image': slide.image4,
+				'price': slide.price4,
+				'salePrice': slide.salePrice4,
+			},
+			// 5
+			{
+				'title': slide.title5,
+				'image': slide.image5,
+				'price': slide.price5,
+				'salePrice': slide.salePrice5,
+			}
+		];
 
 		function jsx(html, ...args) {
 			return html.slice(1).reduce((str, elem, i) => str + args[i] + elem, html[0]);
@@ -189,96 +171,95 @@ export default async function decorate(block) {
 
 		// make jsx for each profile's products panel
 		const productsContainer = document.createElement('div');
-		productsContainer.className = "products-container"
+		var productCounter = 0;
 
 		slideProductsData.forEach(product => {
-			// make product wrapper
+			// make individual product wrapper
 			const productWrapper = document.createElement('div');
 
 			// process product image from curalate link
 			const productPicture = document.createElement('picture')
-
 			productPicture.innerHTML = jsx`
 				<source sizes="352.25px" srcset="${product.image}=/w/150?typ=webp 150w,${product.image}/w/300?typ=webp 300w,${product.image}/w/450?typ=webp 450w,${product.image}/w/600?typ=webp 600w" type="image/webp">
 				<source sizes="352.25px" srcset="${product.image}=/w/150? 150w,${product.image}/w/300? 300w,${product.image}/w/450? 450w,${slide.profileImage}/w/600? 600w" type="image/webp">
 				<img alt="..." src="https://edge.curalate.com/sites/anntaylor-wcpeme/experiences/custom-carousel-1597850128186/assets/imagePlaceholder.png">
 			`;
 
-			// if product info is not empty, render product details;
-			if(product.title !== ''){
+			// if product info is not empty, set innerHtml with product details;
+			if(product.title !== '' && product.image !== ''){
+				productCounter ++;
 				productWrapper.innerHTML = `
 					<div class="product-image">
 						${productPicture.outerHTML}
 					</div>
 					<div class="product-content">
 						<p>${product.title}</p>
+						<p class="prices"><span>${product.price}</span><span>${product.salePrice}</span></p> 
 					</div>
 				`;
 			} else {
+				// product info is empty, set innerHtml as empty
 				productWrapper.innerHTML = null;
+				productWrapper.className = "empty";
 			}
+
+			if(product.salePrice){
+				const sale = productWrapper.querySelector('.prices');
+				sale.classList.add('has-sale');
+			}
+
 			productsContainer.append(productWrapper)
+			productsContainer.className = `products-container`;
 		});
 
-		// handle profile image from curalate link
-		const socialPicture = document.createElement('picture')
-
+		// process profile image from curalate link
+		const socialPicture = document.createElement('picture');
 		socialPicture.innerHTML = jsx`
-		<source sizes="352.25px" srcset="${slide.profileImage}=/w/150?typ=webp 150w,${slide.profileImage}/w/300?typ=webp 300w,${slide.profileImage}/w/450?typ=webp 450w,${slide.profileImage}/w/600?typ=webp 600w" type="image/webp">
-			  <source sizes="352.25px" 
-		srcset="${slide.profileImage}=/w/150? 150w,${slide.profileImage}/w/300? 300w,${slide.profileImage}/w/450? 450w,${slide.profileImage}/w/600? 600w" type="image/webp">
-		<img alt="..." 
-			src="https://edge.curalate.com/sites/anntaylor-wcpeme/experiences/custom-carousel-1597850128186/assets/imagePlaceholder.png">
+			<source sizes="352.25px" srcset="${slide.profileImage}=/w/150?typ=webp 150w,${slide.profileImage}/w/300?typ=webp 300w,${slide.profileImage}/w/450?typ=webp 450w,${slide.profileImage}/w/600?typ=webp 600w" type="image/webp">
+			<source sizes="352.25px" srcset="${slide.profileImage}=/w/150? 150w,${slide.profileImage}/w/300? 300w,${slide.profileImage}/w/450? 450w,${slide.profileImage}/w/600? 600w" type="image/webp">
+			<img alt="..." src="https://edge.curalate.com/sites/anntaylor-wcpeme/experiences/custom-carousel-1597850128186/assets/imagePlaceholder.png">
 		`;
-		// // //
-
-		const togglePopout = (isOpen) => {
-			// isOpen is true (popout is open)
-			if(isOpen){
-				// toggle class to hide div
-				// set data attribute to opposite of isOpen (false)
-			} else {
-				// popout is closed, toggle class & dataAttribute
-			}
-		}
 		
+		// slide rendering structure
 		const createdSlide = document.createElement('li');
 		createdSlide.dataset.slideIndex = idx;
 		createdSlide.setAttribute('id', `carousel-${carouselId}-slide-${idx}`);
 		createdSlide.classList.add('carousel-slide');
-			
+		
 		createdSlide.innerHTML = `
-			<div class="slide-image">
-				${socialPicture.outerHTML}
-			</div>
-			<div class="profile-hover">
-				likes	handle
-				shop the look
-			</div>
-			<div class="popout-wrapper">
+			<button class="popout-trigger">
+				<div class="slide-image">
+					${socialPicture.outerHTML}
+					<div class="profile-hover">
+						<p><img class="icon" src="/icons/filled-heart.svg"/>${slide.likes}</p>
+						<p>${slide.handle}</p>
+						<p class="underline">Shop The Look</p>
+					</div>
+				</div>
+			</button>
+			<div class="popout-wrapper" id="slide-${idx}-popout">
 				<div class="popout-header">
 					<h4>Shop The Look</h4>
-					<div>${slide.likes} ${slide.handle}</div>
-					<button class="close-popout" onClick={clickToClose}>
-						insert close icon here
+					<div>
+						<p><img class="icon" src="/icons/filled-heart.svg" />${slide.likes}</p>
+						<p>${slide.handle}</p>
+					</div>
+					<button class="close-popout">
+						<img class="icon" src="/icons/close.svg" />
 					</button>
-				${productsContainer.outerHTML}
-				<div class="new-arrivals">
-					<h4>New Arrivals</h4>
-					<p>Just-in styles, right this way...</p>
-					<a href="https://www.anntaylor.com/new-arrivals/cata00008?loc=Curalate_NewArrivals&ICID=Curalate_NewArrivals">
-						Shop Now
-					</a>
+				</div>
+				<div class="popout-content products-${productCounter}-up">
+					${productsContainer.outerHTML}
+					<div class="new-arrivals">
+						<h4>New Arrivals</h4>
+						<p>Just-in styles, right this way...</p>
+						<a href="https://www.anntaylor.com/new-arrivals/cata00008?loc=Curalate_NewArrivals&ICID=Curalate_NewArrivals">
+							Shop Now
+						</a>
+					</div>
 				</div>
 			</div>
-			`;
-
-		const labeledBy = createdSlide.querySelector('h1, h2, h3, h4, h5, h6');
-		if (labeledBy) {
-			createdSlide.setAttribute('aria-labelledby', labeledBy.getAttribute('id'));
-		}
-
-		slidesWrapper.append(createdSlide);
+		`;
 
 		if (slideIndicators) {
 			const indicator = document.createElement('li');
@@ -288,13 +269,47 @@ export default async function decorate(block) {
 			slideIndicators.append(indicator);
 		}
 
+		const togglePopout = () => {
+			console.log("toggle function", idx)
+			// const allPopouts = [...block.querySelectorAll(".popout-wrapper")];
+			// console.log("all popouts", allPopouts);
+			// let currentPopout = allPopouts.findIndex(popout => {
+			// 	// find popout that matches slide
+			// 	popout.id == `slide-${idx}-popout`;
+			// })
+			// console.log(currentPopout);
+			// check if active
+	
+				// if not active 
+					// close all other slide popouts
+					// set to active
+	
+				// if active, set to not active
+		};
+
+		const closePopout = () => {
+			console.log("close popout", idx)
+		}
+
+		const popoutTrigger = createdSlide.querySelector(".popout-trigger");
+		popoutTrigger.onclick = function(){togglePopout()}
+	
+	
+		const closeButton = createdSlide.querySelector(".close-popout");
+		closeButton.onclick = function(){closePopout()}
+
 		slidesWrapper.append(createdSlide);
-	})
+	});
 
-  container.append(slidesWrapper);
-  block.prepend(container);
 
-  if(!isSingleSlide) {
-    bindEvents(block);
-  }
+
+
+
+
+	container.append(slidesWrapper);
+	block.prepend(container);
+
+	if(!isSingleSlide) {
+		bindEvents(block);
+	}
 }
